3eebec8a99702d08423590efb017ad173efc73f8
sjkim <07nevergiveup08@gmail.com>
2025-01-30 16:21:37 +0900

    커스텀 데코레이터 적용, 컨트롤러 전체에 인터셉터 적용

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,16 +8,15 @@ import {
   Patch,
   Post,
   Query,
-  UseInterceptors,
-  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { SerializeInterceptor } from './interceptors/serialize.interceptor';
+import { Serialize } from './interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -27,7 +26,6 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     // 쿼리스트링으로 오는 문자열은 number 타입이 아니라 string 타입
