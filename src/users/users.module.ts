2b0aaf312b806d68c2bd4dcce4aa4a86d335958f
sjkim <07nevergiveup08@gmail.com>
2025-01-31 21:40:59 +0900

    회원가입, 로그인 서비스 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])], //리포지토리 자동 생성
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
 })
 export class UsersModule {}
