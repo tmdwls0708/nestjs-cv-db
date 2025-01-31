ff11d859ac2b4c260afe990889523b8562222ad7
sjkim <07nevergiveup08@gmail.com>
2025-01-31 21:41:29 +0900

    쿠키-세션 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+const cookieSession = require('cookie-session'); // nestjs tsconfig 설정 때문에 commonjs 방식으로 import
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.use(
+    cookieSession({
+      keys: ['aaaaaa'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // DTO에 선언된 속성이 아니면 body에 포함되지 않음
